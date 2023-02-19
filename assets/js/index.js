c1e08dc90ae196c34fe3b55b3a2386b8bb3065b1
sharif1014 <sharifuliuk@gmail.com>
2023-02-19 15:59:30 +0600

    final touch

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,11 +6,13 @@
   --------------------------------------------------------------*/
 
   $(window).on('load', function () {
-    
+     isotopeInitilizer();
+
   });
 
   $(function () {
-    hamburger();
+   hamburger();
+   carouselFun();
   });
 
   $(window).on('scroll', function () {
@@ -20,12 +22,59 @@
   /*--------------------------------------------------------------
     1. Test Plugin
   --------------------------------------------------------------*/
+  // Mobile menu functionality
   function hamburger() {
-    $(".hamburger").click(function(){
+    $(".navbar-toggler").click(function(){
       $(".hamburger_icon").toggleClass("close");
     })
   }
 
-
+// Masonry isotope functionality
+ function isotopeInitilizer() {
+  $("#portfolio_items").isotope({
+    // options
+    itemSelector:".portfolio_item",
+    percentPosition: true,
+    masonry: {
+      columnWidth:1,
+    },
+  })
+}
+ //Owl carousel functionality
+ function carouselFun() {
+  $(".client_items").owlCarousel({
+    loop: true,
+    autoplay: true,
+    responsiveClass: true,
+    dots:true,
+    margin:10,
+    responsive: {
+      390: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+  $(".logo_items").owlCarousel({
+    loop: true,
+    autoplay: true,
+    responsiveClass: true,
+    dots:true,
+    margin:15,
+    responsive: {
+      390: {
+        items: 2,
+      },
+     768: {
+        items: 4,
+      },
+      1000: {
+        items:6 ,
+      },
+    },
+  });
+}
 
 })(jQuery); // End of use strict
